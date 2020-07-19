5f702d9573b1ec1841b0eab439f25d57c8b1f56d
Clobotics <50095603+clobotics@users.noreply.github.com>
2020-07-19 17:08:04 +0800

    Add files via upload

---
+++ src/lib/detectors/detector_factory.py
@@ -0,0 +1,17 @@
+from __future__ import absolute_import
+from __future__ import division
+from __future__ import print_function
+
+from .exdet import ExdetDetector
+from .ddd import DddDetector
+from .ctdet import CtdetDetector
+from .multi_pose import MultiPoseDetector
+from .ctdet_angle import CtdetAngleDetector
+
+detector_factory = {
+  'exdet': ExdetDetector, 
+  'ddd': DddDetector,
+  'ctdet': CtdetDetector,
+  'multi_pose': MultiPoseDetector,
+  'ctdet_angle': CtdetAngleDetector
+}
